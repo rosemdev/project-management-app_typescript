d5b7c56c938b80243cbdb3185f65c7cae77125ec
rosemdev <romasemenyshyn@gmail.com>
2024-03-13 18:28:07 +0200

    Edited export files

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,3 @@
-/// <reference path="interfaces/drag-drop.ts"/>
-/// <reference path="models/project.ts"/>
-/// <reference path="state/project-state.ts"/>
-/// <reference path="utils/validation.ts"/>
-/// <reference path="decorators/autobind.ts"/>
 /// <reference path="components/project-list.ts"/>
 /// <reference path="components/project-input.ts"/>
 
